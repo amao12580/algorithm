4b200877a9733d69bda461f2b92febad04cf11e6
Steven Cheng <chengliangchengliang888@gmail.com>
2016-06-30 16:09:01 +0800

    修复快速排序bug

--- src/main/java/classic/middleElement/SolutionV1.java
+++ src/main/java/classic/middleElement/SolutionV1.java
@@ -1,5 +1,10 @@
 package classic.middleElement;
 
+import basic.sort.Sortable;
+import sort.quick.QuickHandler;
+
+import java.util.Arrays;
+
 /**
  * 快速从无序数组中找到中位数
  * <p>
@@ -8,10 +13,13 @@ package classic.middleElement;
  * 思路：
  * <p>
  * 1.排序后取中间位置
- * <p>
- * 2.利用小根堆来解决问题
  */
 public class SolutionV1 {
-
-
+    public static void main(String[] args) throws Exception {
+        Comparable originArray[] = {113, 19, 0, 5, 12, 8, 7, 4, 11, 2, 6, 21, 9};
+        Sortable sortable = new QuickHandler();
+        originArray = sortable.sort(originArray);
+        System.out.println("sorted:" + Arrays.toString(originArray));
+        System.out.println("middle:" + originArray[originArray.length / 2].toString());
+    }
 }
--- src/main/java/sort/quick/QuickHandler.java
+++ src/main/java/sort/quick/QuickHandler.java
@@ -15,8 +15,8 @@ import java.util.Arrays;
 public class QuickHandler extends SortHandlerBehavior {
 
     /**
-     *  思路:
-     *
+     * 思路:
+     * <p>
      * 快速排序时基于分治模式处理的,
      * 对一个典型子数组A[p...r]排序的分治过程为三个步骤:
      * 1.分解:
@@ -24,80 +24,78 @@ public class QuickHandler extends SortHandlerBehavior {
      * A[p ..q-1] <= A[q] <= A[q+1 ..r]
      * 2.解决:通过递归调用快速排序,对子数组A[p ..q-1]和A[q+1 ..r]排序。
      * 3.合并。
-     *
-     *
+     * <p>
+     * <p>
      * 时间复杂度:O(n*log(n))
      *
-     *
-     *
-     * @param originArray  原始输入数组
+     * @param originArray 原始输入数组
      * @return 排好序的数组
-     * @throws Exception  在输入参数不可解析时抛出
+     * @throws Exception 在输入参数不可解析时抛出
      */
     @Override
     public Comparable[] sort(Comparable[] originArray) throws Exception {
         init(originArray);
-        int len=originArray.length;
-        if(len==1){
+        int len = originArray.length;
+        if (len == 1) {
             return originArray;
         }
-        if(len==2){
+        if (len == 2) {
             swapIfLessThan(originArray, 1, 0);
             return originArray;
         }
-        sort(originArray,0,originArray.length-1);
+        sort(originArray, 0, originArray.length - 1);
         return originArray;
     }
-    public void sort(Comparable[] originArray,final int beginIndex,final int endIndex) throws Exception {
-        if((endIndex-beginIndex)<=1){
+
+    public void sort(Comparable[] originArray, final int beginIndex, final int endIndex) throws Exception {
+        if ((endIndex - beginIndex) <= 0) {
             return;
         }
 
-        int N=partition(originArray,beginIndex,endIndex);
+        int N = partition(originArray, beginIndex, endIndex);
         //System.out.println("N:" + N);
-        if((N-1)>=beginIndex){
+        if ((N - 1) >= beginIndex) {
             //System.out.println("N2:" + (N-1));
             sort(originArray, beginIndex, N - 1);
         }
-        if((N+1)<=endIndex){
+        if ((N + 1) <= endIndex) {
             //System.out.println("N3:" + (N+1));
-            sort(originArray,N+1,endIndex);
+            sort(originArray, N + 1, endIndex);
         }
     }
 
     /**
-     *
      * 对数组元素进行移动，切分数组为两部分，使左半部分<=右半部分。
-     *
+     * <p>
      * 左右两部分的分隔元素，下标值记为：N
      *
      * @param originArray 原始数组
-     * @param beginIndex 切分数组的起始元素下标
-     * @param endIndex 切分数组的结束元素下标
+     * @param beginIndex  切分数组的起始元素下标
+     * @param endIndex    切分数组的结束元素下标
      * @return N
      * @throws Exception
      */
 
-    public int partition (Comparable[] originArray ,final int beginIndex,final int endIndex) throws Exception {
+    public int partition(Comparable[] originArray, final int beginIndex, final int endIndex) throws Exception {
         //System.out.println("begin:"+beginIndex+",end:"+endIndex);
-        int basicIndex=beginIndex;
-        int leftIndex=basicIndex+1;
-        int rightIndex=endIndex;
-        while(rightIndex>=leftIndex){
-            while(leftIndex<=endIndex && rightIndex>=leftIndex){
-                if(Comparator.isLT(originArray[leftIndex], originArray[basicIndex])){
+        int basicIndex = beginIndex;
+        int leftIndex = basicIndex + 1;
+        int rightIndex = endIndex;
+        while (rightIndex >= leftIndex) {
+            while (leftIndex <= endIndex && rightIndex >= leftIndex) {
+                if (Comparator.isLT(originArray[leftIndex], originArray[basicIndex])) {
                     swapAlways(originArray, leftIndex, originArray[leftIndex], basicIndex, originArray[basicIndex]);
-                    basicIndex=leftIndex;
-                    leftIndex=basicIndex+1;
-                }else{
+                    basicIndex = leftIndex;
+                    leftIndex = basicIndex + 1;
+                } else {
                     break;
                 }
             }
-            while(beginIndex<=rightIndex && rightIndex>=leftIndex){
-                if(Comparator.isLT(originArray[rightIndex], originArray[basicIndex])){
+            while (beginIndex <= rightIndex && rightIndex >= leftIndex) {
+                if (Comparator.isLT(originArray[rightIndex], originArray[basicIndex])) {
                     swapAlways(originArray, rightIndex, originArray[rightIndex], leftIndex, originArray[leftIndex]);
                     break;
-                }else{
+                } else {
                     rightIndex--;
                 }
             }
@@ -107,15 +105,12 @@ public class QuickHandler extends SortHandlerBehavior {
     }
 
 
-
-
     public static void main(String[] args) throws Exception {
-        Sortable sortable=new QuickHandler();
-        Integer f[]={13,19,9,5,12,8,7,4,11,2,6,21};
-        Integer f2[]={59,-10,0,6,-1,0,8,58,10,2,3,7,8,9,10,12,15,-5,4};
-        System.out.println("---:"+ Arrays.toString(sortable.sort(f)));
+        Sortable sortable = new QuickHandler();
+        Integer f[] = {13, 19, 9, 5, 12, 8, 7, 4, 11, 2, 6, 21};
+        Integer f2[] = {59, -10, 0, 6, -1, 0, 8, 58, 10, 2, 3, 7, 8, 9, 10, 12, 15, -5, 4};
+        System.out.println("---:" + Arrays.toString(sortable.sort(f)));
         System.out.println("---:" + Arrays.toString(sortable.sort(f2)));
-        benchmark(sortable);
+        //benchmark(sortable);
     }
-
 }
