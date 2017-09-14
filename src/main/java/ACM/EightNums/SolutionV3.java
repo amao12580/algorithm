6789374c179d59be1e74afbb31fffd79b2ce9757
Steven Cheng <chengliangchengliang888@gmail.com>
2017-09-14 11:41:30 +0800

    Eight num problem sloved by A*.

--- src/main/java/ACM/EightNums/SolutionV3.java
+++ src/main/java/ACM/EightNums/SolutionV3.java
@@ -1,13 +1,11 @@
 package ACM.EightNums;
 
 import basic.Util;
+import basic.dataStructure.graph.Line;
 
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -29,13 +27,14 @@ import java.util.concurrent.ConcurrentHashMap;
  * 样例输出：
  * 31
  */
-public class SolutionV2 {
+public class SolutionV3 {
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         PrintStream print = new PrintStream("E:\\test.txt");  //写好输出位置文件；
         System.setOut(print);
-        new SolutionV2().case1();
-        new SolutionV2().case2();
-//        new SolutionV2().case3();
+        new SolutionV3().case1();
+//        new SolutionV3().case2();
+//        new SolutionV3().case3();
+//        new SolutionV3().case4();
     }
 
 
@@ -63,6 +62,58 @@ public class SolutionV2 {
         slide((byte) 4, (byte) 4, before, after);
     }
 
+    private void case4() throws FileNotFoundException, InterruptedException {
+        int[] before = {8, 7, 1, 5, 2, 6, 3, 4, 0};
+        int[] after = {8, 7, 1, 6, 2, 5, 3, 4, 0};
+        slide((byte) 3, (byte) 3, before, after);
+    }
+
+    private void init() {
+        //初始化最短距离二维表
+        distanceArray = new int[sum][sum];
+        for (int i = 0; i < sum; i++) {
+            for (int j = 0; j < sum; j++) {
+                distanceArray[i][j] = calculateShortestDistance0(i, j);
+            }
+        }
+    }
+
+    private int calculateShortestDistance0(int small, int big) {
+        if (big == small) {
+            return 0;
+        }
+        int lr = small / columns;
+        int rr = big / columns;
+        if (lr == rr) {
+            return big - small;
+        }
+        return Math.abs(big - rr * columns - (small - lr * columns)) + (rr - lr);
+    }
+
+    private int calculateShortestDistance(int small, int big) {
+        return distanceArray[small][big];
+    }
+
+    private int calculateShortestDistance(int[] descV2I, int[] afterV2I) {
+        int d = 0;
+        for (int i = 0; i < sum; i++) {
+            d += calculateShortestDistance(descV2I[i], afterV2I[i]);
+        }
+        return d;
+    }
+
+    private int calculateShortestDistance(int[] current, int lastDistance, int lastIndex, int currentIndex, int[] target) {
+        int tbi = target[current[lastIndex]];
+        int tai = target[current[currentIndex]];
+        return lastDistance
+                - (calculateShortestDistance(tbi, currentIndex))
+                - (calculateShortestDistance(tai, lastIndex))
+                + (calculateShortestDistance(tbi, lastIndex))
+                + (calculateShortestDistance(tai, currentIndex));
+    }
+
+    private int[][] distanceArray;
+
     /**
      * 有rows行，每行有columns个
      */
@@ -72,16 +123,17 @@ public class SolutionV2 {
         this.rows = rows;
         this.columns = columns;
         this.sum = rows * columns;
-        String beforeString = contactAll(before);
-        String afterString = contactAll(after);
-        List<Current> moves = new ArrayList<>();
-        moves.add(new Current(indexOfZero(before), (byte) -1, beforeString.toCharArray()));
-        positiveCurrents.put(beforeString, 0);
-        negativeCurrents.put(afterString, 0);
-        positiveThread = new SearchHandler(moves, true);
-        moves = new ArrayList<>();
-        moves.add(new Current(indexOfZero(after), (byte) -1, afterString.toCharArray()));
-        negativeThread = new SearchHandler(moves, false);
+        init();
+        Set<Current> moves = new HashSet<>();
+        int[] afterV2I = V2I(after);
+        int[] beforeV2I = V2I(before);
+        moves.add(new Current(indexOfZero(before), -1, before, beforeV2I, calculateShortestDistance(beforeV2I, afterV2I)));
+        positiveCurrents.put(Arrays.toString(before), 0);
+        negativeCurrents.put(Arrays.toString(before), 0);
+        positiveThread = new SearchHandler(moves, true, after);
+        moves = new HashSet<>();
+        moves.add(new Current(indexOfZero(after), -1, after, afterV2I, calculateShortestDistance(afterV2I, beforeV2I)));
+        negativeThread = new SearchHandler(moves, false, before);
         positiveThread.start();
         negativeThread.start();
         print();
@@ -94,10 +146,10 @@ public class SolutionV2 {
     private Thread positiveThread;
     private Thread negativeThread;
 
-    private byte indexOfZero(int[] array) {
+    private int indexOfZero(int[] array) {
         for (int i = 0; i < sum; i++) {
             if (array[i] == 0) {
-                return (byte) i;
+                return i;
             }
         }
         return -1;
@@ -110,7 +162,7 @@ public class SolutionV2 {
             System.out.println("shortest path length is " + globalMin);
             System.out.println("--------------------------------------------");
         } else {
-            Thread.sleep(100);
+            Thread.sleep(20);
             print();
         }
     }
@@ -136,6 +188,14 @@ public class SolutionV2 {
         return false;
     }
 
+    private int[] V2I(int[] array) {
+        int[] result = new int[sum];
+        for (int i = 0; i < sum; i++) {
+            result[array[i]] = i;
+        }
+        return result;
+    }
+
     private int getShortestSteps(boolean isPositive, String current) {
         return (isPositive ? positiveCurrents : negativeCurrents).getOrDefault(current, 0);
     }
@@ -143,10 +203,12 @@ public class SolutionV2 {
     class SearchHandler extends Thread {
         private boolean isPositive;
         private List<Current> moves;
+        private int[] targetV2I;
 
-        SearchHandler(List<Current> moves, boolean isPositive) {
-            this.moves = moves;
+        SearchHandler(Set<Current> moves, boolean isPositive, int[] targetI2V) {
+            this.moves = new ArrayList<>(moves);
             this.isPositive = isPositive;
+            this.targetV2I = V2I(targetI2V);
         }
 
         @Override
@@ -156,17 +218,19 @@ public class SolutionV2 {
 
         private void slide(List<Current> moves) {
             Current current;
-            char[] currentDesc;
+            int[] currentDescI2V, currentDescV2I;
             List<Byte> canMove;
-            List<Current> next = new ArrayList<>();
+            Set<Current> next = new HashSet<>();
             for (Current item : moves) {
-//                System.out.println(isPositive + "," + String.valueOf(item.getDesc()));
+                System.out.println(isPositive + "," + Arrays.toString(item.getDescI2V()));
                 canMove = canMove(item.getWhiteIndex(), item.getPreWhiteIndex());
                 for (Byte move : canMove) {
                     current = item.copyOf();
-                    currentDesc = Arrays.copyOf(current.getDesc(), sum);
-                    current.plusStep(currentDesc, move(currentDesc, current.getWhiteIndex(), move));
-                    if (check(String.valueOf(currentDesc), current.getStep())) {
+                    currentDescI2V = Arrays.copyOf(current.getDescI2V(), sum);
+                    currentDescV2I = Arrays.copyOf(current.getDescV2I(), sum);
+                    current.plusStep(currentDescI2V, currentDescV2I, move(currentDescI2V,
+                            currentDescV2I, current.getWhiteIndex(), move), targetV2I);
+                    if (check(Arrays.toString(currentDescI2V), current.getStep())) {
                         next.add(current);
                     } else {
                         current.clear();
@@ -178,7 +242,12 @@ public class SolutionV2 {
             if (next.isEmpty()) {
                 return;
             }
-            slide(next);
+            List<Current> temp = new ArrayList<>(next);
+            next.clear();
+            if (next.size() > 1) {
+                Collections.sort(temp);
+            }
+            slide(temp);
         }
 
         synchronized boolean check(String currentString, int steps) {
@@ -186,7 +255,7 @@ public class SolutionV2 {
             boolean crossed = cross(isPositive, currentString);
             if (crossed) {
                 steps = getShortestSteps(isPositive, currentString) + getShortestSteps(!isPositive, currentString);
-//                System.out.println("may be " + steps + ",currentString:" + currentString);
+                System.out.println("may be " + steps + ",currentString:" + currentString);
                 if (globalMin < 0 || steps < globalMin) {
                     globalMin = steps;
                 }
@@ -200,18 +269,9 @@ public class SolutionV2 {
         }
     }
 
-
     private int globalMin = -1;
 
-    private String contactAll(int[] array) {
-        StringBuilder builder = new StringBuilder(sum);
-        for (int item : array) {
-            builder = builder.append(item);
-        }
-        return builder.toString();
-    }
-
-    private List<Byte> canMove(byte whiteIndex, byte block) {
+    private List<Byte> canMove(int whiteIndex, int block) {
         List<Byte> result = new ArrayList<>(4);
         //上
         if (whiteIndex >= columns) {
@@ -240,57 +300,68 @@ public class SolutionV2 {
         return result;
     }
 
-    private byte move(char[] current, byte whiteIndex, byte direction) {
+    private int move(int[] I2V, int[] V2I, int whiteIndex, int direction) {
         switch (direction) {
             case 0:
-                swapArray(current, whiteIndex, whiteIndex - columns);
-                return (byte) (whiteIndex - columns);
+                swapArray(I2V, V2I, whiteIndex, whiteIndex - columns);
+                return whiteIndex - columns;
             case 1:
-                swapArray(current, whiteIndex, whiteIndex - 1);
-                return (byte) (whiteIndex - 1);
+                swapArray(I2V, V2I, whiteIndex, whiteIndex - 1);
+                return whiteIndex - 1;
             case 2:
-                swapArray(current, whiteIndex, whiteIndex + columns);
-                return (byte) (whiteIndex + columns);
+                swapArray(I2V, V2I, whiteIndex, whiteIndex + columns);
+                return whiteIndex + columns;
             case 3:
-                swapArray(current, whiteIndex, whiteIndex + 1);
-                return (byte) (whiteIndex + 1);
+                swapArray(I2V, V2I, whiteIndex, whiteIndex + 1);
+                return whiteIndex + 1;
             default:
                 throw new IllegalArgumentException();
         }
     }
 
-    private void swapArray(char[] arrays, int thisIndex, int otherIndex) {
-        char temp = arrays[thisIndex];
+    private void swapArray(int[] I2V, int[] V2I, int thisIndex, int otherIndex) {
+        swapArray(V2I, I2V[thisIndex], I2V[otherIndex]);
+        swapArray(I2V, thisIndex, otherIndex);
+    }
+
+    private void swapArray(int[] arrays, int thisIndex, int otherIndex) {
+        int temp = arrays[thisIndex];
         arrays[thisIndex] = arrays[otherIndex];
         arrays[otherIndex] = temp;
     }
 
-    class Current {
-        private byte whiteIndex;
-        private byte preWhiteIndex = -1;
-        private char[] desc;
+    class Current implements Comparable<Current> {
+        private int whiteIndex;
+        private int preWhiteIndex = -1;
+        private int hashCode;
+        private int[] descI2V;
+        private int[] descV2I;
         private int steps;
+        private int distance;
 
 
-        Current(byte whiteIndex, byte preWhiteIndex, char[] desc) {
+        Current(int whiteIndex, int preWhiteIndex, int[] descI2V, int[] descV2I, int distance) {
             if (whiteIndex < 0) {
                 throw new IllegalArgumentException();
             }
             this.whiteIndex = whiteIndex;
             this.preWhiteIndex = preWhiteIndex;
-            this.desc = desc;
+            this.descI2V = descI2V;
+            this.hashCode = Arrays.toString(descI2V).hashCode();
+            this.descV2I = descV2I;
+            this.distance = distance;
         }
 
-        Current(byte whiteIndex, byte preWhiteIndex, char[] desc, int steps) {
-            this(whiteIndex, preWhiteIndex, desc);
+        Current(int whiteIndex, int preWhiteIndex, int[] descI2V, int[] descV2I, int distance, int steps) {
+            this(whiteIndex, preWhiteIndex, descI2V, descV2I, distance);
             this.steps = steps;
         }
 
-        byte getWhiteIndex() {
+        int getWhiteIndex() {
             return whiteIndex;
         }
 
-        byte getPreWhiteIndex() {
+        int getPreWhiteIndex() {
             return preWhiteIndex;
         }
 
@@ -298,23 +369,54 @@ public class SolutionV2 {
             return steps;
         }
 
-        public char[] getDesc() {
-            return desc;
+        int[] getDescI2V() {
+            return descI2V;
+        }
+
+        int[] getDescV2I() {
+            return descV2I;
         }
 
         Current copyOf() {
-            return new Current(this.whiteIndex, this.preWhiteIndex, this.desc, this.steps);
+            return new Current(this.whiteIndex, this.preWhiteIndex, this.descI2V, this.descV2I, this.distance, this.steps);
         }
 
-        void plusStep(char[] desc, byte newIndex) {
+        void plusStep(int[] descI2V, int[] descV2I, int newIndex, int[] targetV2I) {
             this.preWhiteIndex = whiteIndex;
+            this.distance = calculateShortestDistance(descI2V, this.distance, this.preWhiteIndex, newIndex, targetV2I);
             this.whiteIndex = newIndex;
-            this.desc = desc;
+            this.descI2V = descI2V;
+            this.hashCode = Arrays.toString(descI2V).hashCode();
+            this.descV2I = descV2I;
             this.steps++;
         }
 
         void clear() {
-            this.desc = null;
+            this.descI2V = null;
+            this.descV2I = null;
+        }
+
+        @Override
+        public int compareTo(Current other) {
+            return this.distance - other.distance;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null) {
+                return false;
+            }
+            if (o instanceof Current) {
+                Current other = (Current) o;
+                return this.hashCode() == other.hashCode();
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return hashCode;
         }
     }
 }
